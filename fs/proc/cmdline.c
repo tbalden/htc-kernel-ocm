51fbfc83996f006a6d1b5beb2db9055ab13931d2
tbalden <illespal@gmail.com>
2017-12-16 03:24:31 -0800

    cmdline: verified bootstate check override with magisk ignore
    adding check magisk code to verify if magisk boot image.
    If so do not overried cmdline params, it causes inability
    to boot on Oreo.
    
    If non-magisk, replace android.bootstateverified with aaaa...

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -2,13 +2,48 @@
 #include <linux/init.h>
 #include <linux/proc_fs.h>
 #include <linux/seq_file.h>
-#include <asm/setup.h>
 
-static char new_command_line[COMMAND_LINE_SIZE];
+#if 1
+#include <linux/slab.h>
+#include <linux/spinlock.h>
+
+static bool done = false;
+
+static DEFINE_SPINLOCK(show_lock);
+
+static const char *replace =      "androidboot.verifiedbootstate=";
+static char *replace_with =       "aaaaaaaaaaaaaaaaaaaaaaaaaaaaa=";
+
+static bool magisk = true;
+
+extern bool is_magisk(void);
+extern bool is_magisk_sync(void);
+extern void init_magisk(void);
+#endif
 
 static int cmdline_proc_show(struct seq_file *m, void *v)
 {
-	seq_printf(m, "%s\n", new_command_line);
+#if 1
+	spin_lock(&show_lock);
+	if (done) {
+	} else {
+		magisk = is_magisk();
+		if (!magisk) {
+			char *tmp = saved_command_line;
+			tmp = strstr(tmp,replace);
+			if (tmp) {
+				while ((*replace_with)!='\0') {
+					*tmp = *replace_with;
+					replace_with++;
+					tmp++;
+				}
+			}
+		}
+		done = true;
+	}
+	spin_unlock(&show_lock);
+#endif
+	seq_printf(m, "%s\n", saved_command_line);
 	return 0;
 }
 
@@ -26,29 +61,9 @@ static const struct file_operations cmdline_proc_fops = {
 
 static int __init proc_cmdline_init(void)
 {
-	char *offset_addr, *cmd = new_command_line;
-
-	strcpy(cmd, saved_command_line);
-
-	/*
-	 * Remove 'androidboot.verifiedbootstate' flag from command line seen
-	 * by userspace in order to pass SafetyNet CTS check.
-	 */
-	offset_addr = strstr(cmd, "androidboot.verifiedbootstate=");
-	if (offset_addr) {
-		size_t i, len, offset;
-
-		len = strlen(cmd);
-		offset = offset_addr - cmd;
-
-		for (i = 1; i < (len - offset); i++) {
-			if (cmd[offset + i] == ' ')
-				break;
-		}
-
-		memmove(offset_addr, &cmd[offset + i + 1], len - i - offset);
-	}
-
+#if 1
+	init_magisk();
+#endif
 	proc_create("cmdline", 0, NULL, &cmdline_proc_fops);
 	return 0;
 }
